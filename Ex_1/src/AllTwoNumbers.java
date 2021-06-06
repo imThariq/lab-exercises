081b55b1c9604161b2dd1e66cf7a26801c25f16d
mdtha <mdtha@ImThariq>
2021-06-06 15:40:13 +0530

    Ex_1

---
+++ Ex_1/src/AllTwoNumbers.java
@@ -0,0 +1,12 @@
+
+public class AllTwoNumbers {
+
+	public static void main(String[] args) {
+	int number1 = 50, number2 = 150, sum;
+	 sum = number1 + number2;
+	 System.out.println("Sum of these numbers: "+sum);
+	 {
+		// TODO Auto-generated method stub
+               }
+	}
+}
